3b0a9b8bf7bfd98166646bb2edf42bd8f4ab09ca
Aiden <72966716+aidenbok203@users.noreply.github.com>
2025-03-21 19:35:11 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from random import randint
+
+xpdr = []
+
+xpdr.append(randint(1, 7)
+            
+xpdr.append(randint(1, 7)
+            
+xpdr.append(randint(1, 7)
+            
+xpdr.append(randint(1, 7)
+
+print(xpdr)
